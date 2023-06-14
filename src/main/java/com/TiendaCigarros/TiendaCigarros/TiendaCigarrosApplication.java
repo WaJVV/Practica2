d448b11f02c2298c6369796fe40cda38ec979e00
wayne <wayne@192.168.1.49>
2023-06-14 15:53:56 -0600

    Practica 2

---
+++ src/main/java/com/TiendaCigarros/TiendaCigarros/TiendaCigarrosApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaCigarros.TiendaCigarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaCigarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaCigarrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaCigarros/TiendaCigarros/TiendaCigarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaCigarros.TiendaCigarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaCigarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
